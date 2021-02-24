b41466adae23f71879e88ba67e3c8c34164dbc4b
Pablo Saldarriaga <psaldarr@my.centennialcollege.ca>
2021-02-24 01:12:04 -0500

    added healthbar

---
+++ Assets/_Scripts/HealthBarWorldSpaceController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarWorldSpaceController : MonoBehaviour
+{
+    public Transform playerCam;
+
+    void Start()
+    {
+        playerCam = GameObject.Find("PlayerCam").transform;   
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + playerCam.forward);
+    }
+
+}
